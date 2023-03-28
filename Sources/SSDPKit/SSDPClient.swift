b4eba77ab68ecca1096a5924ee26739540ef7c90
Dariusz Niklewicz <dniklewicz@gmail.com>
2023-03-29 00:54:35 +0200

    Add helper method for starting SSDP scanning with Duration parameter

--- Sources/SSDPKit/SSDPClient.swift
+++ Sources/SSDPKit/SSDPClient.swift
@@ -21,6 +21,12 @@ public class SSDPClient {
     public func startScanning(for duration: TimeInterval) -> AnyPublisher<URL, Error> {
         backend.startScanning(for: duration)
     }
+    
+    @available(macOS 13.0, *)
+    public func startScanning(for duration: Duration) -> AnyPublisher<URL, Error> {
+        let timeInterval = TimeInterval(duration.components.seconds) + Double(duration.components.attoseconds)/1e18
+        return backend.startScanning(for: timeInterval)
+    }
 
     public func stopScanning() {
         backend.stopScanning()
